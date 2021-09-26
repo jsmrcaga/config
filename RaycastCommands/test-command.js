b9327481acf0681d45d80d59adc9262af39f264b
Jo Colina <jo.colina@weezevent.com>
2021-09-26 23:21:17 +0200

    Platform: add Raycast scripts with demo

---
+++ RaycastCommands/test-command.js
@@ -0,0 +1,22 @@
+#!/usr/bin/env node
+
+// Required parameters:
+// @raycast.schemaVersion 1
+// @raycast.title test-me
+// @raycast.mode fullOutput
+
+// Optional parameters:
+// @raycast.icon 🤖
+// @raycast.packageName Jo - Test
+// @raycast.needsConfirmation false
+// @raycast.argument1 { "type": "text", "placeholder": "Arg me!", "percentEncoded": false, "optional": true, "secure": false }
+
+
+
+// Documentation:
+// @raycast.description A command to test raycast commands
+// @raycast.author Jo Colina
+// @raycast.authorURL https://github.com/jsmrcaga
+
+console.log("Hello World!", process.argv)
+
