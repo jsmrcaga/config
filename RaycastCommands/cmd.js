93791d2ae2393926a2b86ead305330d64a91173d
Jo Colina <jo.colina@weezevent.com>
2022-10-30 15:25:45 +0100

    Raycast: unsafe cmd command to run any command from nodejs

---
+++ RaycastCommands/cmd.js
@@ -0,0 +1,32 @@
+#!/usr/bin/env node
+
+// Required parameters:
+// @raycast.schemaVersion 1
+// @raycast.title cmd
+// @raycast.mode fullOutput
+
+// Optional parameters:
+// @raycast.icon 🎮
+// @raycast.packageName Jo - Scripts
+// @raycast.needsConfirmation true
+// @raycast.argument1 { "type": "text", "placeholder": "Command", "percentEncoded": false, "optional": false, "secure": false }
+
+
+
+// Documentation:
+// @raycast.description Run any command
+// @raycast.author Jo Colina
+// @raycast.authorURL https://github.com/jsmrcaga
+
+const Process = require('child_process');
+
+const args = process.argv.slice(2); 
+const [command] = args;
+
+if(!command) {
+	throw new Error('No command provided');
+}
+
+
+const result = Process.execSync(command);
+console.log(result.toString('utf8'));
